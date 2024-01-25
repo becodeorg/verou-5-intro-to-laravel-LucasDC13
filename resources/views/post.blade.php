3917ce4a9c715ad48deae4ab5a41c74c7ea377aa
LucasDC13 <lucas_de_corte@hotmail.com>
2024-01-25 16:08:09 +0100

    Add multiple posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!doctype HTML>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wow, a homepage!</title>
+        <link rel="stylesheet" href="/app.css">
+        <script src="/app.js"></script>
+    </head>
+
+    <body>
+        <article>
+            <?= $post ?>
+        </article>        
+        <a href="/"><button>Home</button></a>    
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function() {
-    return view('index');
+    return view('posts');
 });
 
+
+Route::get('posts/{post}', function ($slug) {
+    $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+    return view('post', [
+            'post' => $post
+    ]);
+});
 
 Route::get('/welcome', function () {
     return view('welcome');
